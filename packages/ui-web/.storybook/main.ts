4fe4e17ae1e913b69c0809a96643764f9074990d
AlexandreAlface <21131@stu.ipbeja.pt>
2025-08-12 18:25:03 +0100

    feat: add Storybook configuration and dependencies
    - Updated package.json to include Storybook scripts and dependencies.
    - Enhanced Storybook configuration in ui-web to include additional stories and addons.
    - Modified ui-web package.json to include Storybook-related scripts and peer dependencies.

--- packages/ui-web/.storybook/main.ts
+++ packages/ui-web/.storybook/main.ts
@@ -1,11 +1,12 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.stories.@(ts|tsx)'],
-  addons: [],
   framework: {
     name: '@storybook/react-vite',
     options: {},
   },
+  stories: ['../src/**/*.stories.@(ts|tsx|mdx)'],
+  addons: ['@storybook/addon-essentials', '@storybook/addon-interactions'],
+  docs: { autodocs: 'tag' },
 };
 export default config;
